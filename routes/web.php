292bd5f92746f6f0af228f89fd126d7877a94c3d
viernism <jejevier1337@gmail.com>
2023-06-15 11:12:53 +0700

    readme

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,6 @@ use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\UserProfileController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::middleware(['guest'])->group(function () {
     Route::get('register',[RegisterController::class,'index']);
     Route::post('register',[RegisterController::class,'registerUser'])->name('register');
